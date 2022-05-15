f8b0b977517847f5c08fefa851f6e2f908082a0a
liufr <liufengrui18z@ict.ac.cn>
2022-05-15 23:52:59 +0800

    update xstream test

--- test/test_xstream.py
+++ test/test_xstream.py
@@ -1,9 +1,20 @@
-from streamad.util import StreamGenerator, UnivariateDS
+from streamad.util import StreamGenerator, UnivariateDS, MultivariateDS
 from streamad.model import xStreamDetector
 
 
 def test_score():
     ds = UnivariateDS()
+    stream = StreamGenerator(ds.data)
+    detector = xStreamDetector()
+    for x in stream.iter_item():
+        score = detector.fit_score(x)
+
+        if score is not None:
+            assert 0 <= score <= 1
+
+
+def test_multi_score():
+    ds = MultivariateDS()
     stream = StreamGenerator(ds.data)
     detector = xStreamDetector()
     for x in stream.iter_item():
