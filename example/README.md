af94d33c94b8e76ece1126e17d5dd46b2dec164e
Brandon Fergerson <bfergerson@apache.org>
2022-06-21 12:08:25 +0400

    correct documentation relax type hint

--- example/README.md
+++ example/README.md
@@ -1,3 +1,3 @@
 This folder contains the symlink of all examples from docs/source/examples/
 
-If it is fail to open in some certain OS, plase check the origin file or the online docs.
+If it is fail to open in some certain OS, please check the origin file or the online docs.
--- streamad/process/tdigest_Thresholder.py
+++ streamad/process/tdigest_Thresholder.py
@@ -8,7 +8,7 @@ from collections import deque
 class TDigestThresholder:
     def __init__(
         self,
-        detector: Type[BaseDetector],
+        detector: BaseDetector,
         percentile_up: float = 95,
         percentile_down: float = 5,
         is_global: bool = True,
@@ -17,9 +17,9 @@ class TDigestThresholder:
         """A thresholder which can filter out outliers using t-digest, and normalize the anomaly scores into [0,1] :cite:`DBLP:journals/simpa/Dunning21`.
 
         Args:
-            detector (Type[BaseDetector]): A detector that must be a child class of BaseDetector.
-            percentile_up (float, optional): We regard the scores that beyond `percentile_up` as anomalies. Defaults to 95.
-            percentile_down (float, optional): We regard the scores that below `percentile_up` as anomalies. Defaults to 5.
+            detector (BaseDetector): A detector that must be a child class of BaseDetector.
+            percentile_up (float, optional): We regard the scores above `percentile_up` as anomalies. Defaults to 95.
+            percentile_down (float, optional): We regard the scores below `percentile_down` as anomalies. Defaults to 5.
             is_global (bool, optional): Method to record, a global way or a rolling window way. Defaults to True.
             window_len (int, optional): The length of rolling window, ignore this when `is_global=True`. Defaults to 100.
         """
