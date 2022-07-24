da36ea5bffd4b14ec79cc1e9954ff51c099a04fc
Fengrui-Liu <liufengrui18z@ict.ac.cn>
2022-07-24 21:01:43 +0800

    Func: theta-method for periodicity

--- streamad/process/__init__.py
+++ streamad/process/__init__.py
@@ -2,10 +2,12 @@ from .zscore_calibrator import ZScoreCalibrator
 from .tdigest_calibrator import TDigestCalibrator
 from .weight_ensemble import WeightEnsemble
 from .vote_ensemble import VoteEnsemble
+from .theta_periodicity import ThetaMultiPeriodicity
 
 __all__ = [
     "ZScoreCalibrator",
     "TDigestCalibrator",
     "WeightEnsemble",
     "VoteEnsemble",
+    "ThetaMultiPeriodicity",
 ]
